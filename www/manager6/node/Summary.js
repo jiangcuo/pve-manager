f6ab483851a718ed909a197bcdf169280a0d3071
jiangcuo <jiangcuo@lierfang.com>
2025-03-08 11:16:12 +0800

    fix node submmary size

--- www/manager6/node/Summary.js
+++ www/manager6/node/Summary.js
@@ -150,7 +150,7 @@ Ext.define('PVE.node.Summary', {
 		    layout: 'column',
 		    minWidth: 700,
 		    defaults: {
-			minHeight: 350,
+			minHeight: 320,
 			padding: 5,
 			columnWidth: 1,
 		    },
